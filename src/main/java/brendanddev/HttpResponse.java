0e5b5f2e8f4cdceefb428b8389b33d38f59560a6
Brendan Dileo <dileob23@gmail.com>
2025-08-15 10:02:43 -0400

    Implement http response class

--- src/main/java/brendanddev/HttpResponse.java
+++ src/main/java/brendanddev/HttpResponse.java
@@ -1,5 +1,28 @@
 package brendanddev;
 
+/**
+ * Represents an HTTP response, including the response body, status code, and status text.
+ * 
+ * This class is used by the HTTP server to encapsulate both the content to send to the client
+ * and the corresponding HTTP status.
+ */
 public class HttpResponse {
+
+    String body;
+    int statusCode;
+    String statusText;
+
+    /**
+     * Constructs a new HttpResponse object with the specified body, status code, and status text.
+     * 
+     * @param body The response content to send to the client.
+     * @param statusCode The HTTP status code.
+     * @param statusText The HTTP status text corresponding to the status code.
+     */
+    public HttpResponse(String body, int statusCode, String statusText) {
+        this.body = body;
+        this.statusCode = statusCode;
+        this.statusText = statusText;
+    }
     
 }
