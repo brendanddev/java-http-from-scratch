4c5a08d06ba10854989b8e87416b3a36062e04f8
Brendan Dileo <dileob23@gmail.com>
2025-08-15 16:51:07 -0400

    Setup class and setup get method

--- src/main/java/brendanddev/HttpClient.java
+++ src/main/java/brendanddev/HttpClient.java
@@ -1,5 +1,33 @@
 package brendanddev;
 
+import java.net.Socket;
+
+/**
+ * A simple HTTP client implemented from scratch using sockets
+ */
 public class HttpClient {
+
+    private String host;
+    private int port;
+
+    /**
+     * Constructs a new HttpClient that connects to the specified host and port.
+     * 
+     * @param host The hostname or IP address of the server to connect to.
+     * @param port The port number on which the server is listening.
+     */
+    public HttpClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+
+    public String get(String path) throws Exception {
+
+        try (Socket socket = new Socket(host, port)) {
+            
+        }
+
+    }
     
 }
