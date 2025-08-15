86a95712692444c286e53cfd98b3a7cca4f8118d
Brendan Dileo <dileob23@gmail.com>
2025-08-15 09:02:24 -0400

    Update main with basic execution logic

--- src/main/java/brendanddev/Main.java
+++ src/main/java/brendanddev/Main.java
@@ -1,1 +1,18 @@
 package brendanddev;
+
+/**
+ * The main entry point for the HTTP server application.
+ */
+public class Main {
+
+
+    /**
+     * Main method to start the HTTP server
+     * @param args Unused.
+     */
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer(8080);
+        server.start();
+    }
+    
+}
