633d6a7a4591f7bafdea7f522532c9c30eb34af5
Brendan Dileo <dileob23@gmail.com>
2025-08-15 14:19:53 -0400

    Add helper method to send get requests from client

--- src/test/java/brendanddev/HttpServerTest.java
+++ src/test/java/brendanddev/HttpServerTest.java
@@ -1,7 +1,41 @@
 package brendanddev;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+/**
+ * Unit tests for the HttpServer class
+ */
 public class HttpServerTest {
+
+    /**
+     * Sends a simple HTTP GET request to the given URL and returns the first line of the response.
+     * 
+     * @param urlStr The URL to send the GET request to.
+     * @return The first line of the response body.
+     * @throws Exception If an error occurs while sending the request or reading the response.
+     */
+    private String sendGetRequest(String urlStr) throws Exception {
+        // Create URL object and open connection
+        URL url = java.net.URI.create(urlStr).toURL();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        // Read one line of the response
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            return in.readLine();
+        }
+    }
+
     
 }
