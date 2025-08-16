8c3e6615dbf4fbf893b30e2dcb50d2a90f3f4ebd
Brendan Dileo <dileob23@gmail.com>
2025-08-16 12:27:35 -0400

    Update main client execution class

--- src/main/java/brendanddev/client/MainClient.java
+++ src/main/java/brendanddev/client/MainClient.java
@@ -1,8 +1,42 @@
 package brendanddev.client;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
- * 
+ * Main entry point for accessing the HTTP client.
  */
 public class MainClient {
+
+    public static void main(String[] args) {
+
+        try {
+            // Load client properties from the configuration file
+            Properties props = new Properties();
+            try (InputStream in = MainClient.class.getResourceAsStream("/client.properties")) {
+                props.load(in);
+            }
+
+            String host = props.getProperty("server.host", "localhost");
+            int port = Integer.parseInt(props.getProperty("server.port", "8080"));
+
+            // Create instance of HttpClient with the loaded properties
+            HttpClient client = new HttpClient(host, port);
+
+            // Try GET request to the root path
+            String response = client.get("/");
+            System.out.println("Response from server:");
+            System.out.println(response);
+
+            // Try POST request to the submit path
+            String postResponse = client.post("/submit", "Hello from client!", "text/plain");
+            System.out.println("Response from POST request:");
+            System.out.println(postResponse);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
     
 }
